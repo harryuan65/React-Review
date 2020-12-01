56387e73cbcba6dc7a04b51e994d0b91f489d080
Harry Yuan <harryuan.65@gmail.com>
2020-12-01 23:41:57 +0800

    feat: import css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import "./Person.css"
 const person = (props) => {
   return(
-    <div>
+    <div className="Person">
       <p onClick={props.click}>Hi, I'm {props.name}. I'm {props.age}</p>
       <p>{props.children}</p>
       <input onChange={props.changed} value={props.name}></input>
